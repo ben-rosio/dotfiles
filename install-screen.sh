5f45be9f4d73fbfcd136af83966cc5cc8d53b322
ben <ben@ros.io>
2015-02-12 11:49:44 -0700

    Make screen download quiet

--- install-screen.sh
+++ install-screen.sh
@@ -1,4 +1,4 @@
-wget http://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz
+wget -q http://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz
 tar -xzf screen-4.2.1.tar.gz
 rm screen-4.2.1.tar.gz
 cd screen-4.2.1
