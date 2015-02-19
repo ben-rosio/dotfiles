5659ff75d4f819f838e9bca863cd18be8c244fea
ben <ben@ros.io>
2015-02-19 11:35:22 -0700

    Update to use fresh

--- install.sh
+++ install.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
-
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-
-##########
-
-ln -s $dir/bashrc ~/.bashrc
-ln -s $dir/vimrc ~/.vimrc
-mkdir ~/.vagrant.d
-ln -s $dir/vagrant.d/Vagrantfile ~/.vagrant.d/Vagrantfile
-ln -s $dir/screenrc ~/.screenrc
-ln -s $dir/gitconfig ~/.gitconfig
-
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall > /dev/null
+FRESH_LOCAL_SOURCE=ben-rosio/dotfiles bash -c "`curl -sL get.freshshell.com`"
