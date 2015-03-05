9d3cd85c0fd8138f94755995af49534732766523
ben <ben@ros.io>
2015-03-05 14:42:56 -0700

    Update projects command to have autocomplete

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,4 +1,3 @@
 alias ip='curl curlmyip.com -q'
 
 alias cpv="rsync -poghb --backup-dir//tmp/rsync -e /dev/null --progress --"
-alias projects="cd ~/Projects"
---
+++ shell/functions/projects.sh
@@ -0,0 +1,11 @@
+_projects ()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(ls ~/Projects)" -- $cur) )
+}
+complete -F _projects projects
+
+projects ()
+{
+    cd ~/Projects/$1
+}
