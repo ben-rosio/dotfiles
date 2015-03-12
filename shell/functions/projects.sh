1bd481b29755ff6bcfde57f52944f8c2fab3c00b
ben <ben@ros.io>
2015-03-12 11:18:54 -0600

    Fix projects command definition

--- shell/functions/projects.sh
+++ shell/functions/projects.sh
@@ -1,11 +1,9 @@
-_projects ()
-{
+_projects() {
     local cur=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=( $(compgen -W "$(ls ~/Projects)" -- $cur) )
 }
 complete -F _projects projects
 
-projects ()
-{
+projects() {
     cd ~/Projects/$1
 }
