877f90c69fb76eab13f8904e84e137ca6ad350f3
ben <ben@ros.io>
2015-03-15 16:04:48 -0600

    Map awesome tags to numbers and numberpad

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -333,46 +333,50 @@ clientkeys = awful.util.table.join(
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it works on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
+local np_map = { 87, 88, 89, 83, 84, 85, 79, 80, 81 }
 for i = 1, 9 do
-    globalkeys = awful.util.table.join(globalkeys,
-        -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = mouse.screen
-                        local tag = awful.tag.gettags(screen)[i]
-                        if tag then
-                           awful.tag.viewonly(tag)
-                        end
-                  end),
-        -- Toggle tag.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = mouse.screen
-                      local tag = awful.tag.gettags(screen)[i]
-                      if tag then
-                         awful.tag.viewtoggle(tag)
-                      end
-                  end),
-        -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = awful.tag.gettags(client.focus.screen)[i]
+    for j = 1, 2 do
+        key = "#" .. (j == 1 and i + 9 or np_map[i])
+        globalkeys = awful.util.table.join(globalkeys,
+            -- View tag only.
+            awful.key({ modkey }, key,
+                      function ()
+                            local screen = mouse.screen
+                            local tag = awful.tag.gettags(screen)[i]
+                            if tag then
+                               awful.tag.viewonly(tag)
+                            end
+                      end),
+            -- Toggle tag.
+            awful.key({ modkey, "Control" }, key,
+                      function ()
+                          local screen = mouse.screen
+                          local tag = awful.tag.gettags(screen)[i]
                           if tag then
-                              awful.client.movetotag(tag)
+                             awful.tag.viewtoggle(tag)
                           end
-                     end
-                  end),
-        -- Toggle tag.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = awful.tag.gettags(client.focus.screen)[i]
-                          if tag then
-                              awful.client.toggletag(tag)
+                      end),
+            -- Move client to tag.
+            awful.key({ modkey, "Shift" }, key,
+                      function ()
+                          if client.focus then
+                              local tag = awful.tag.gettags(client.focus.screen)[i]
+                              if tag then
+                                  awful.client.movetotag(tag)
+                              end
+                         end
+                      end),
+            -- Toggle tag.
+            awful.key({ modkey, "Control", "Shift" }, key,
+                      function ()
+                          if client.focus then
+                              local tag = awful.tag.gettags(client.focus.screen)[i]
+                              if tag then
+                                  awful.client.toggletag(tag)
+                              end
                           end
-                      end
-                  end))
+                      end))
+    end
 end
 
 clientbuttons = awful.util.table.join(
