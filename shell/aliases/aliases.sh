840b0f78ad9092cce2e12ea52bbc354c5f04ae06
ben <ben@ros.io>
2015-05-20 12:10:51 -0600

    find use extended regex

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,5 +1,7 @@
 alias ip='curl curlmyip.com -q'
 
-alias cpv="rsync -poghb --backup-dir /tmp/rsync -e /dev/null --progress --"
+alias cpv='rsync -poghb --backup-dir /tmp/rsync -e /dev/null --progress --'
 
 alias env="env | grep -iv 'secret' | grep -iv 'password'"
+
+alias find='find -E'
