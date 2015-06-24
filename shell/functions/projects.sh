5c6273b74a2898bc6355cc7336c8612498d7f2d3
ben <ben@ros.io>
2015-06-24 14:02:49 -0600

    Update projects to p, use PROJECT_DIRECTORY env var

--- shell/functions/projects.sh
+++ shell/functions/projects.sh
@@ -1,9 +1,9 @@
 _projects() {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(ls ~/Projects)" -- $cur) )
+    COMPREPLY=( $(compgen -W "$(ls $PROJECT_DIRECTORY)" -- "$cur") )
 }
-complete -F _projects projects
+complete -F _projects p
 
-projects() {
-    cd ~/Projects/$1
+p() {
+    cd $PROJECT_DIRECTORY/$1
 }
--- shell/globalrc.sh
+++ shell/globalrc.sh
@@ -2,3 +2,6 @@
 if [ -f /etc/bashrc ]; then
         . /etc/bashrc
 fi
+
+# Set normal environment variables
+export PROJECT_DIRECTORY=~/Projects/
