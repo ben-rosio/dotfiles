a52dbb8c38bb055917322a0e096bd4f805f2a92b
ben <ben@ros.io>
2015-06-30 08:10:22 -0600

    Add elixer alias

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -7,3 +7,5 @@ alias cpv='rsync -poghb --backup-dir /tmp/rsync -e /dev/null --progress --'
 alias env="env | grep -iv 'secret' | grep -iv 'password'"
 
 alias find='find -E'
+
+alias elixer='echo Did you mean iex?' # Because fuck iex as a command
