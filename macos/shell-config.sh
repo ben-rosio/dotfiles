fbbf67a8de9aecd5c4b56081d614f27a8338f27f
ben <ben@ros.io>
2015-08-10 10:45:29 -0600

    Don't automatically switch spaces if app gains focus

--- macos/shell-config.sh
+++ macos/shell-config.sh
@@ -3,3 +3,6 @@ defaults write com.apple.screencapture disable-shadow -bool true
 
 # Fixes finder preview showing blank
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Don't change space when app gains focus
+defaults write com.apple.Dock workspaces-auto-swoosh -bool false
